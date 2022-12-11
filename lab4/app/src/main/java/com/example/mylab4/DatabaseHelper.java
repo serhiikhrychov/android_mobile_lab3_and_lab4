428b41924ec7923b93f21632abad8bb66b0e44bb
serhiichrychov <sergeyhall@gmail.com>
2022-12-11 16:51:40 +0200

    Add lab4 part1

---
+++ lab4/app/src/main/java/com/example/mylab4/DatabaseHelper.java
@@ -0,0 +1,51 @@
+package com.example.mylab4;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "people.db";
+    public static final String TABLE_NAME = "people_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "EMAIL";
+    public static final String COL_4 = "TVSHOW";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "NAME TEXT, EMAIL TEXT, TVSHOW TEXT)";
+        sqLiteDatabase.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    public boolean addData(String name, String email, String tvShow) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, email);
+        contentValues.put(COL_4, tvShow);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
--- lab4/app/src/main/java/com/example/mylab4/MainActivity.java
+++ lab4/app/src/main/java/com/example/mylab4/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.mylab4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper peopleDB;
+    Button btnAddData;
+    EditText etName, etEmail, etTvShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        peopleDB = new DatabaseHelper(this);
+
+        etName = findViewById(R.id.etNewName);
+        etEmail = findViewById(R.id.etNewEmail);
+        etTvShow = findViewById(R.id.etNewTVShow);
+        btnAddData = findViewById(R.id.btnAddData);
+
+        AddData();
+    }
+
+    public void AddData() {
+        btnAddData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean isInserted = peopleDB.addData(etName.getText().toString(), etEmail.getText().toString(), etTvShow.getText().toString());
+                if (isInserted = true) {
+                    Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Data Not Inserted", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
